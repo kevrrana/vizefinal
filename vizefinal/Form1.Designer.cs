133a51932d329017386dbeb407a6644f6fe657f9
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-10-17 14:10:50 +0300

    Add files via upload

---
+++ vizefinal/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace vizefinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVize = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(94, 159);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(211, 31);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vize: ";
+            // 
+            // txtVize
+            // 
+            this.txtVize.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtVize.Location = new System.Drawing.Point(94, 72);
+            this.txtVize.Name = "txtVize";
+            this.txtVize.Size = new System.Drawing.Size(211, 31);
+            this.txtVize.TabIndex = 2;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtFinal.Location = new System.Drawing.Point(94, 122);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(211, 31);
+            this.txtFinal.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Final: ";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(39, 267);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(94, 25);
+            this.lblDurum.TabIndex = 6;
+            this.lblDurum.Text = "Durum: ";
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOrtalama.Location = new System.Drawing.Point(12, 217);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(121, 25);
+            this.lblOrtalama.TabIndex = 5;
+            this.lblOrtalama.Text = "Ortalama: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblOrtalama);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtVize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtVize;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label lblOrtalama;
+    }
+}
+
---
+++ vizefinal/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vizefinal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            float final, vize,ortalama;
+            final = Convert.ToSingle(txtFinal.Text);
+            vize = Convert.ToSingle(txtVize.Text);
+            ortalama = vize * 0.4f + final * 0.6f;
+
+            lblOrtalama.Text = "Ortalama: " + ortalama;
+            if (ortalama >= 50 && final >= 50)
+            {
+                lblDurum.Text = "Durum: Geçtiniz";
+            }
+            else
+            {
+                lblDurum.Text = "Durum: Kaldınız:(";
+            }
+        }
+    }
+}
